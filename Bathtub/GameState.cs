3af7e3cf78d42293b8ca195dad2f5ede6de973a7
RexProcellas <ngcmason@gmail.com>
2026-04-18 19:01:59 -0400

    Added documentation comments

--- Bathtub/GameState.cs
+++ Bathtub/GameState.cs
@@ -2,6 +2,14 @@ using SFML.Graphics;
 
 public struct GameState(string text, bool shouldContinue)
 {
+    /// <summary>
+    /// If the game state should not continue, this text will be displayed in the banner above the grid.
+    /// </summary>
     public string Text { get; } = text;
+    /// <summary>
+    /// Whether or not this gamestate should trigger another time step.
+    /// If not, the game will display the provided text above the grid 
+    /// in the banner and stop calling <see cref="Game.LogicStep"/>.
+    /// </summary>
     public bool ShouldContinue { get; } = shouldContinue;
 }
